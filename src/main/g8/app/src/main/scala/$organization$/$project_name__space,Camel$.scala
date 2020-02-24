0a6d52a6dcae81ee820036326c62ff682a512afd
Siyavash Habashi <ghashange@live.com>
2020-02-24 22:53:01 +0100

    Setup project.

---
+++ src/main/g8/app/src/main/scala/$organization$/$project_name__space,Camel$.scala
@@ -0,0 +1,9 @@
+package $organization$
+
+object $project_name;format="space,Camel"$ {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Happy hacking!")
+  }
+}
---
+++ src/main/g8/app/src/test/scala/$organization$/$project_name__space,Camel$Spec.scala
@@ -0,0 +1,14 @@
+package $organization$
+
+import org.scalatest.freespec._
+
+class $project_name;format="space,Camel"$ extends AnyFreeSpec {
+
+  "$project_name$" - {
+
+    "should succeed." in {
+
+      succeed
+    }
+  }
+}
